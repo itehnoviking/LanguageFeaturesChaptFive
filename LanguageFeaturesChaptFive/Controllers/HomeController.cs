cf2bda28da6345997b7e5b70177718a3ece4153b
Иван К. PC <itehnoviking@gmail.com>
2022-01-15 17:01:30 +0300

    Added model Product, Controller Index and View

--- LanguageFeaturesChaptFive/Controllers/HomeController.cs
+++ LanguageFeaturesChaptFive/Controllers/HomeController.cs
@@ -15,7 +15,16 @@ namespace LanguageFeaturesChaptFive.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add(string.Format($"Name:{name}, Price:{price}, Related:{relatedName}"));
+
+            }
+            return View(results);
         }
 
         public IActionResult Privacy()
---
+++ LanguageFeaturesChaptFive/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace LanguageFeaturesChaptFive.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }    
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product odnaKopeyka1844em = new Product
+            {
+                Name = "1 копейка серебром 1844 г. Е.М.",
+                Price = 1600M
+            };
+
+            Product polushka1736 = new Product
+            {
+                Name = "Полушка 1736 г.",
+                Price = 180M
+            };
+
+            Product polushka1735 = new Product
+            {
+                Name = "Полушка 1735 г.",
+                Price = 170M
+            };
+
+            polushka1735.Related = polushka1736;
+            polushka1736.Related = polushka1735;
+
+            return new Product[] { odnaKopeyka1844em, polushka1736, polushka1735, null };
+        }
+    }
+}
